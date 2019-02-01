549c02471bd4c7107f8ff65e39e629da492bc559
Thierry Bugier <tbugier@teclib.com>
2019-02-01 16:07:32 +0100

    build: bump version
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- setup.php
+++ setup.php
@@ -31,14 +31,14 @@
 
 global $CFG_GLPI;
 // Version of the plugin
-define('PLUGIN_FORMCREATOR_VERSION', '2.8.0');
+define('PLUGIN_FORMCREATOR_VERSION', '2.9.0-nightly');
 // Schema version of this version
-define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.8');
+define('PLUGIN_FORMCREATOR_SCHEMA_VERSION', '2.9');
 // is or is not an official release of the plugin
 define('PLUGIN_FORMCREATOR_IS_OFFICIAL_RELEASE', false);
 
 // Minimal GLPI version, inclusive
-define ('PLUGIN_FORMCREATOR_GLPI_MIN_VERSION', '9.3.0');
+define ('PLUGIN_FORMCREATOR_GLPI_MIN_VERSION', '9.4');
 // Maximum GLPI version, exclusive
 define ('PLUGIN_FORMCREATOR_GLPI_MAX_VERSION', '9.5');
 
@@ -56,7 +56,7 @@ function plugin_version_formcreator() {
       return false;
    }
    $requirements = [
-      'name'           => 'Form Creator',
+      'name'           => _n('Form', 'Forms', 2, 'formcreator'),
       'version'        => PLUGIN_FORMCREATOR_VERSION,
       'author'         => '<a href="http://www.teclib.com">Teclib\'</a>',
       'homepage'       => 'https://github.com/pluginsGLPI/formcreator',
