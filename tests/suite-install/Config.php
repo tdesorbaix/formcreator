b5943fdf7786c721c5bbd99da5a0c1977a03cb66
Thierry Bugier <tbugier@teclib.com>
2019-02-26 14:54:46 +0100

    refactor(targetticket,targetchange): remove enums
    impaced fields
    - due_date_rule
    - due_date_period
    - urgency_rule
    
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- tests/suite-install/Config.php
+++ tests/suite-install/Config.php
@@ -109,16 +109,13 @@ class Config extends CommonTestCase {
       $this->checkConfig();
       $this->testPluginName();
 
-      // Enable debug mode for enrollment messages
-      \Config::setConfigurationValues($pluginname, ['debug_enrolment' => '1']);
+      // // Take a snapshot of the database before any test
+      // $this->mysql_dump($DB->dbuser, $DB->dbhost, $DB->dbpassword, $DB->dbdefault, './save.sql');
 
-      // Take a snapshot of the database before any test
-      $this->mysql_dump($DB->dbuser, $DB->dbhost, $DB->dbpassword, $DB->dbdefault, './save.sql');
-
-      $this->boolean(file_exists("./save.sql"))->isTrue();
-      $filestats = stat("./save.sql");
-      $length = $filestats[7];
-      $this->integer($length)->isGreaterThan(0);
+      // $this->boolean(file_exists("./save.sql"))->isTrue();
+      // $filestats = stat("./save.sql");
+      // $length = $filestats[7];
+      // $this->integer($length)->isGreaterThan(0);
    }
 
    public function testUpgradePlugin() {
